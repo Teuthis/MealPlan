edc6930a99a482948b98b97866774732fb327431
Chris Aiken <chris.aiken@outlook.com>
2017-09-10 11:57:34 -0400

    Recipe class skeleton created

---
+++ MealPlan/Recipe.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlan
+{
+    class Recipe
+    {
+        private string name;
+        private string link;
+        private int prepTime;
+        private int naughtiness;
+        private int cost;
+        private int heaviness;
+        private bool leftovers;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        public string RecipeLink
+        {
+            get { return link; }
+            set { link = value; }
+        }
+
+        public int PreparationTime
+        {
+            get { return prepTime; }
+            set { prepTime = Math.Min( Math.Max( 1, value ), 5 ); }
+        }
+
+        public int Naughtiness
+        {
+            get { return naughtiness; }
+            set { naughtiness = Math.Min( Math.Max( 1, value ), 5 ); }
+        }
+
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = Math.Min( Math.Max( 1, value ), 5 ); }
+        }
+
+        public int Heaviness
+        {
+            get { return heaviness; }
+            set { heaviness = Math.Min( Math.Max( 1, value ), 5 ); }
+        }
+
+        public bool ProducesLeftovers
+        {
+            get { return leftovers; }
+            set { leftovers = value; }
+        }
+
+    }
+}
