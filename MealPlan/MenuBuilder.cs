c4324262ebc7e808e40b0e1a8c2b994b5fb8d59d
Chris Aiken <chris.aiken@outlook.com>
2017-09-11 09:29:04 -0400

    Added MenuBuilder class skeleton

---
+++ MealPlan/MenuBuilder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlan
+{
+    class MenuBuilder
+    {
+        List<Recipe> pool;
+        List<Recipe> menu;
+        List<bool> pins;
+
+
+        public double AveragePrepTime
+        {
+            get
+            {
+                double sum = 0.0;
+                foreach (var meal in menu) {
+                    sum += meal.PreparationTime;
+                }
+                return sum / menu.Count;
+            }
+        }
+
+        public double AverageCost
+        {
+            get
+            {
+                double sum = 0.0;
+                foreach (var meal in menu) {
+                    sum += meal.Cost;
+                }
+                return sum / menu.Count;
+            }
+        }
+
+        public double AverageNaughtiness
+        {
+            get
+            {
+                double sum = 0.0;
+                foreach (var meal in menu) {
+                    sum += meal.Naughtiness;
+                }
+                return sum / menu.Count;
+            }
+        }
+
+        public double AverageHeaviness
+        {
+            get
+            {
+                double sum = 0.0;
+                foreach (var meal in menu) {
+                    sum += meal.Heaviness;
+                }
+                return sum / menu.Count;
+            }
+        }
+
+        public int LeftoversCount
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var meal in menu) {
+                    if (meal.ProducesLeftovers) sum++;
+                }
+                return sum;
+            }
+        }
+    }
+}
