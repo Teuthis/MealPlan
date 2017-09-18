fc66d0697100305a997b141674834791517c825e
Chris Aiken <chris.aiken@outlook.com>
2017-09-18 10:59:07 -0400

    Added Meal rating value range unit tests

--- MealPlan_Tests/Meal_Test.cs
+++ MealPlan_Tests/Meal_Test.cs
@@ -17,5 +17,117 @@ namespace MealPlan_Tests
             int actual = meal.PreparationTime;
             Assert.AreEqual( expected, actual );
         }
+
+        [TestMethod]
+        public void PrepValueLowClamp() {
+            Meal meal = new Meal();
+            meal.PreparationTime = -3;
+            int expected = 1;
+            int actual = meal.PreparationTime;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void PrepValueInRange() {
+            Meal meal = new Meal();
+            meal.PreparationTime = 1;
+            int expected = 1;
+            int actual = meal.PreparationTime;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void CostValueHighClamp() {
+            Meal meal = new Meal();
+            meal.Cost = 43;
+            int expected = 5;
+            int actual = meal.Cost;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void CostValueLowClamp() {
+            Meal meal = new Meal();
+            meal.Cost = -3;
+            int expected = 1;
+            int actual = meal.Cost;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void CostValueInRange() {
+            Meal meal = new Meal();
+            meal.Cost = 1;
+            int expected = 1;
+            int actual = meal.Cost;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void NaughtyValueHighClamp() {
+            Meal meal = new Meal();
+            meal.Naughtiness = 43;
+            int expected = 5;
+            int actual = meal.Naughtiness;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void NaughtyValueLowClamp() {
+            Meal meal = new Meal();
+            meal.Naughtiness = -3;
+            int expected = 1;
+            int actual = meal.Naughtiness;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void NaughtyValueInRange() {
+            Meal meal = new Meal();
+            meal.Naughtiness = 1;
+            int expected = 1;
+            int actual = meal.Naughtiness;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void HeavyValueHighClamp() {
+            Meal meal = new Meal();
+            meal.Heaviness = 43;
+            int expected = 5;
+            int actual = meal.Heaviness;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void HeavyValueLowClamp() {
+            Meal meal = new Meal();
+            meal.Heaviness = -3;
+            int expected = 1;
+            int actual = meal.Heaviness;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void HeavyValueInRange() {
+            Meal meal = new Meal();
+            meal.Heaviness = 1;
+            int expected = 1;
+            int actual = meal.Heaviness;
+            Assert.AreEqual( expected, actual );
+        }
+
+        [TestMethod]
+        public void ConstructorRangeCorrectness() {
+            Meal meal = new Meal( "Test meal", "", -3, 43, 19, 0, false );
+            int expectedCost = 1;
+            int expectedPrep = 5;
+            int expectedNaughty = 5;
+            int expectedHeavy = 1;
+            Assert.AreEqual( expectedCost, meal.Cost );
+            Assert.AreEqual( expectedPrep, meal.PreparationTime );
+            Assert.AreEqual( expectedNaughty, meal.Naughtiness );
+            Assert.AreEqual( expectedHeavy, meal.Heaviness );
+        }
     }
 }
